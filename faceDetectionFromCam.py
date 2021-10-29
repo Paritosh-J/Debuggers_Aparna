22b7796e7383d778e8ece2cbfcc0ba93c8139230
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2021-10-29 22:55:10 +0530

    Update faceDetectionFromCam.py

--- faceDetectionFromCam.py
+++ faceDetectionFromCam.py
@@ -1,23 +1,5 @@
 import cv2, dlib, time
 
-def captureScreenshot(frame, face):
-    # Display the alert on screen
-    cv2.putText(frame, "ALERT! NO MORE SPACE AVAILABLE", (45, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4)
-
-    # draw rectangle around the face
-    for f in face:
-        x, y = f.left(), f.top()
-        a, b = f.right(), f.bottom()
-        cv2.rectangle(frame, (x, y), (a, b), (0, 0, 255), 2) 
-
-    # show the captured ss
-    cv2.imshow('!!! MORE THAN 3 PASSENGERS DETECTED !!!', frame)
-    
-    # press any key to destroy the window
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 def captureVid():
     # live cam face detection
     faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -48,9 +30,8 @@ def captureVid():
                 flag = 1
 
             if count > 1:  # if more than 3 persons in 2 wheeler vehicle
-                time.sleep(0.5)
                 # function call to capture the snap of 3 or more faces
-                captureScreenshot(frame, face)
+                cv2.putText(frame, "ALERT! Defaulter Found", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 4)
 
             # coordinates for drawing rectangle around face
             x, y = f.left(), f.top()
