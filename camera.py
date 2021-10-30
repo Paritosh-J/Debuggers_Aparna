e21092b8cf8faa4d1795668bb0f151660aa35a44
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2021-10-30 18:19:17 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,53 +1,55 @@
-import cv2, dlib
+import cv2
 
+# faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-class Video(object):
-    #def __init__(self):
-        #self.video=cv2.VideoCapture(0)
-    #def __del__(self):
-        #self.video.release()
+class Video(object):        
+    def captureScreenshot(img, face):
+        # Display the alert on screen
+        cv2.putText(img, "ALERT! NO MORE SPACE AVAILABLE", (45, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4)
+        # draw rectangle around the detected face
+        for (x, y, w, h) in face:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2) 
+        # show the captured ss
+        cv2.imshow('!!! MORE THAN 3 PASSENGERS DETECTED !!!', img)
+            
+            
     def get_frame(self):
-        # capture the frame
-        faceDetector = dlib.get_frontal_face_detector()
-        # print(frame)
-        while True:
-            video = cv2.VideoCapture(-1)
-            # check, frame = video.read()
-            ret, frame = self.video.read()
-            # flip the frame captured like a mirror image
-            frame = cv2.flip(frame, 1)
-            # convert the frame into gray scale image (optional)
-            grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # detect face
-            face = faceDetector(grayImg)
-            # variable with intial count for no. of faces = 0
-            count = 0
-            # flag for face limit warning
-            flag = 0
+        # get/read the image
+        img = cv2.imread('faces.jpg', 1)
+        # print(img)  # check if image read successfully
 
-            for f in face:
-                # checking face limit
-                if count == 1:
-                    cv2.putText(frame, "WARNING! Max limit reached", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 4)
+        # detect face by decreasing the scaleFactor by 1.05
+        faces = faceCascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5)
+
+        # initial no. of faces = 0
+        count = 0
+        # flag for facelimit warning
+        flag = 0
+        # draw a rectangle around the detected face
+        for (x, y, w, h) in faces:
+            if count == 1:
+                    cv2.putText(img, "WARNING! Max limit reached", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 4)
                     flag = 1
 
-                if count > 1:  # if more than 3 persons in 2 wheeler vehicle
-                    # function call to capture the snap of 3 or more faces
-                    cv2.putText(frame, "ALERT! Defaulter Found", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 4)
+            if count > 1:  # if more than 3 persons in 2 wheeler vehicle
+                # time.sleep(0.5)
+                # function call to capture the snap of 3 or more faces
+                captureScreenshot(img, faces)
 
-                # coordinates for drawing rectangle around face
-                x, y = f.left(), f.top()
-                a, b = f.right(), f.bottom()
+            # draw rectangle around the face
+            if flag == 1: # 2 faces detected, mark 2nd face with yellow
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)  
+            else:
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-                # draw rectangle around the face
-                if flag == 1: # max faces detected
-                    cv2.rectangle(frame, (x, y), (a, b), (0, 255, 255), 2)  
-                else:
-                    cv2.rectangle(frame, (x, y), (a, b), (255, 0, 0), 2)  
+            # increment face count upon detection
+            count += 1
+            # print text on screen (Face No. : )
+            cv2.putText(img, "Face No." + str(count), (x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2) # color format : BGR
 
-                # increment face count upon detection
-                count += 1
-            ret,jpg=cv2.imencode('.jpg',frame)
-            return jpg.tobytes()
+        # open window for showing the o/p
+        cv2.imshow('pic', img)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
