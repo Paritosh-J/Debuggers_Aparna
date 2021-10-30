de00117182d4f133b67d3b8f1a81331d03d9b0de
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2021-10-30 18:33:44 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,18 +3,10 @@ import cv2
 # faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-class Video(object):        
-    def captureScreenshot(img, face):
-        # Display the alert on screen
-        cv2.putText(img, "ALERT! NO MORE SPACE AVAILABLE", (45, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4)
-        # draw rectangle around the detected face
-        for (x, y, w, h) in face:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2) 
-        # show the captured ss
-        cv2.imshow('!!! MORE THAN 3 PASSENGERS DETECTED !!!', img)
-            
-            
+class Video(object):
     def get_frame(self):
+         # face detector initialized   
+        faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         # get/read the image
         img = cv2.imread('faces.jpg', 1)
         # print(img)  # check if image read successfully
@@ -24,29 +16,16 @@ class Video(object):
 
         # initial no. of faces = 0
         count = 0
-        # flag for facelimit warning
-        flag = 0
         # draw a rectangle around the detected face
         for (x, y, w, h) in faces:
-            if count == 1:
-                    cv2.putText(img, "WARNING! Max limit reached", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 4)
-                    flag = 1
-
-            if count > 1:  # if more than 3 persons in 2 wheeler vehicle
-                # time.sleep(0.5)
-                # function call to capture the snap of 3 or more faces
-                captureScreenshot(img, faces)
-
-            # draw rectangle around the face
-            if flag == 1: # 2 faces detected, mark 2nd face with yellow
-                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)  
-            else:
-                img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
+            # draw rectangle around face
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             # increment face count upon detection
             count += 1
             # print text on screen (Face No. : )
             cv2.putText(img, "Face No." + str(count), (x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2) # color format : BGR
+
+        cv2.putText(img, "Total Faces Detected : " + str(count), (45, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         # open window for showing the o/p
         cv2.imshow('pic', img)
